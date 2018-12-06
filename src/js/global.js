b4b0efeb3aba18b1e2ab493ad4065dae542c3daf
290448666 <290448666@qq.com>
2018-12-06 23:10:32 +0800

    调整整体布局样式

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,18 @@
 (function(globalDataStr) {
+  let navShow = false
+
   if (globalDataStr) {
     window.globalData = JSON.parse(decodeURIComponent(globalDataStr))
   }
+
+  const navBar = document.querySelector('nav')
+  document.querySelector('.icon-caidan').addEventListener('click', () => {
+    if (!navShow) {
+      navBar.style.display = 'flex'
+      navShow = true
+    } else {
+      navBar.style.display = 'none'
+      navShow = false
+    }
+  }, false)
 })(globalDataStr)
